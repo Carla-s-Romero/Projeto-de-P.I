dd5f07692be9133721ffc93e6f81350f399be96a
João Pedro Marins <90799102+VitorMarins@users.noreply.github.com>
2024-09-25 22:12:00 -0300

    ajeitei o login e o registar pq n tava pegando

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,8 +4,8 @@ const config = require('../config/jwt');
 
 exports.registrar = async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const usuario = new Usuario({ username, password });
+    const { nome, email, senha, matricula, tipo } = req.body;
+    const usuario = new Usuario({ nome, email, senha, matricula, tipo });
     await usuario.save();
     res.status(201).json({ message: 'Usuario registered successfully' });
   } catch (error) {
@@ -15,9 +15,9 @@ exports.registrar = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const usuario = await Usuario.findOne({ username });
-    if (!usuario || !(await usuario.comparePassword(password))) {
+    const { nome, senha } = req.body;
+    const usuario = await Usuario.findOne({ nome });
+    if (!usuario || !(await usuario.comparePassword(senha))) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
     const token = jwt.sign({ id: usuario._id }, config.secret, { expiresIn: '1h' });
