58fe4ef64c4695eb1595866bdc20afb423f6cc8b
Lucas Albuquerque <lucas.albuquerque2k@gmail.com>
2024-09-29 09:22:53 -0300

    Todas as Rotas de Tipo de Usuarios Funcionando!

--- backend/routes/usuarios.routes.js
+++ backend/routes/usuarios.routes.js
@@ -3,15 +3,13 @@ const { getUsuarios, getUsuarioById, updateUsuario, deleteUsuario, getProfessore
 const authMiddleware = require('../middleware/auth.middleware');
 const router = express.Router();
 
-//Essas 3 rotas abaixo estão funcionando perfeitamente
+//Todas as rotas estão funcionando perfeitamente :)
 router.get('/', authMiddleware, getUsuarios);
 router.get('/:id', authMiddleware, getUsuarioById);
 router.put('/:id', authMiddleware, updateUsuario);
 router.delete('/:id', authMiddleware, deleteUsuario);
-// Esses 3 abaixo estão dando um erro desgraçado no postman, provavelmente problema no controller ou na rota, não sei
-// o erro é: "error": "Cast to ObjectId failed for value \"aluno\" (type string) at path \"_id\" for model \"Usuarios\""
-router.get('/professore', authMiddleware, getProfessores);
-router.get('/aluno', authMiddleware, getAlunos);
-router.get('/coordenadore', authMiddleware, getCoordenadores);
+router.get('/tipo/professores', authMiddleware, getProfessores);
+router.get('/tipo/alunos', authMiddleware, getAlunos);
+router.get('/tipo/coordenadores', authMiddleware, getCoordenadores);
 
 module.exports = router;
