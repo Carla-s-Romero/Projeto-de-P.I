b7c2c5d7bb1b0312f84446eccfadd23d6214f5bb
Vitor Barbosa Marins <vitorbarbosamarins@gmail.com>
2024-11-12 18:45:55 -0300

    Delete backend directory

--- backend/models/notificacoes.models.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const NotificacoesSchema = new mongoose.Schema({
-    mensagem: { type: String, required: true },
-    data: { type: Date, default: Date.now },
-    autor: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuarios', required: true }
-});
-
-module.exports = mongoose.model('Notificacoes', NotificacoesSchema);
